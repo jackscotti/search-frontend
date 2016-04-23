671a41860953bc127a28a3d250f774ec21108da1
jackscotti <jacopojackscotti@gmail.com>
2016-04-23 08:29:52 +0100

    Add links to items

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,10 +4,17 @@ class SearchController < ApplicationController
     if @search_params = params["q"]
       q = params.values.join("+")
       @results = Faraday.get('https://www.gov.uk/api/search.json?q=' + @search_params)
-      @results = @results.body
-      @results = JSON.parse(@results)
-      @results = @results['results']
-      @results = @results.map {|result| result["title"]}
+      @results = present(@results)
+    end
+  end
+
+
+  def present(results)
+    results = JSON.parse(results.body)
+    results = results['results']
+    results = results.map do |result|
+      result["link"] = "https://www.gov.uk" + result["link"]
+      result.select{|k,v| ["title", "link"].include?(k)}
     end
   end
 end
